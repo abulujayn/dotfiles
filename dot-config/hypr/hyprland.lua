e943cc5b235899fd3300c87b7e549dca72a70f58
abulujayn <zaeem@parkar.au>
2026-09-15 21:22:04 +1000

    turn on force zero scaling

--- dot-config/hypr/hyprland.lua
+++ dot-config/hypr/hyprland.lua
@@ -5,33 +5,36 @@ hl.env("XCURSOR_PATH", "/home/abulujayn/.local/share/icons:/usr/share/icons")
 hl.env("GTK_CSD", "0")
 
 hl.config({
-  general = {
-    layout = "scrolling",
+    general = {
+        layout = "scrolling",
 
-    border_size = 2,
-    gaps_in = 3,
-    gaps_out = 9,
-    col = {
-      active_border = "rgba(81a1c1ff)"
-    }
-  },
-  scrolling = {
-    column_width = 0.5,
-    fullscreen_on_one_column = false,
-    explicit_column_widths = "0.33, 0.5, 1.0",
-    follow_min_visible = 1.0,
-  },
-  decoration = {
-    rounding = 0
-  },
-  input = {
-    touchpad = {
-      natural_scroll = true
-    }
-  },
-  animations = {
-    enabled = false,
-  },
+        border_size = 2,
+        gaps_in = 3,
+        gaps_out = 9,
+        col = {
+            active_border = "rgba(81a1c1ff)"
+        }
+    },
+    scrolling = {
+        column_width = 0.5,
+        fullscreen_on_one_column = false,
+        explicit_column_widths = "0.33, 0.5, 1.0",
+        follow_min_visible = 1.0,
+    },
+    decoration = {
+        rounding = 0
+    },
+    input = {
+        touchpad = {
+            natural_scroll = true
+        }
+    },
+    animations = {
+        enabled = false,
+    },
+    xwayland = {
+      force_zero_scaling = true
+    },
 })
 
 
